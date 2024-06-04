fb84825e0a7d7e9c6628f3b0692f8fbedbaad219
Arjun vilas p <arjunarjus.07@gmail.com>
2024-06-04 23:47:45 +0530

    created header and hero section

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -3,12 +3,12 @@ import './HeroSection.css'
 const HeroSection = () => {
   return (
     <div className='hero-container'>
-        <div className="details">
-            <h1 className='name'>ARJUN VILAS P</h1>
-            <p className='desc'>Aspiring software developer</p>
-        </div>
         <div className="avatar">
             <img src='/avatar2.webp' alt='avatar'/>
+        </div>
+        <div className="details">
+            <h1 className='name'>ARJUN VILAS P</h1>
+            <p className='desc'>software developer</p>
         </div>
     </div>
   )
