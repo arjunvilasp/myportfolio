1fac5466bab470683bd80221aa63c5f13bfebc80
Arjun vilas p <arjunarjus.07@gmail.com>
2024-06-05 12:13:48 +0530

    added roboto font and also made both header and hero section responsive

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -1,15 +1,25 @@
 import './HeroSection.css'
+import {motion} from 'framer-motion'
+
 
 const HeroSection = () => {
   return (
     <div className='hero-container'>
-        <div className="avatar">
-            <img src='/avatar2.webp' alt='avatar'/>
-        </div>
-        <div className="details">
+        <motion.div 
+         initial={{ opacity: 0, x: -150 }}
+         animate={{ opacity: 1, x: 0 }}
+         transition={{ duration: 1 }}
+        className="details">
             <h1 className='name'>ARJUN VILAS P</h1>
             <p className='desc'>software developer</p>
-        </div>
+        </motion.div>
+        <motion.div
+        initial={{ opacity : 0, y : -100}}
+        animate={{ opacity : 1, y : 0}}
+        transition={{duration:1}}
+        className="avatar">
+            <img src='/avatar2.webp' alt='avatar'/>
+        </motion.div>
     </div>
   )
 }
